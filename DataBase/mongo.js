77458f8250bf8195fa0a7a58d58b4a14e2fb0c93
Blake_Whitham <bwhitham77@gmail.com>
2021-03-24 17:25:39 -0700

    Mongo Atlas rework

--- DataBase/mongo.js
+++ DataBase/mongo.js
@@ -1,10 +1,8 @@
 const { MongoClient } = require('mongodb');
 
-console.log(process.env.REACT_APP_ENTRY_KEY);
-
 const uri = `mongodb+srv://user:${process.env.REACT_APP_ENTRY_KEY}@multistepcheckout.mum9y.mongodb.net/mortgage?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-module.exports.db = client.connect();
+client.connect();
 module.exports.client = client;
